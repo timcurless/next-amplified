a10e2933f18cd140359de32a7f5de47cd90e9d7b
Tim Curless <tim.curless@ahead.com>
2023-11-18 14:20:14 -0600

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import ConfigureAmplifyClientSide from '@/app/components/ConfigureAmplifyClientSide';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <>
+          <ConfigureAmplifyClientSide />
+          {children}
+        </>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,69 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+import { generateServerClientUsingCookies } from '@aws-amplify/adapter-nextjs/api';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+
+import * as mutations from '@/src/graphql/mutations';
+import * as queries from '@/src/graphql/queries';
+
+import config from '@/src/amplifyconfiguration.json';
+
+export const  cookiesClient = generateServerClientUsingCookies({
+  config,
+  cookies
+});
+
+async function createTodo(formData: FormData) {
+  'use server';
+
+  await cookiesClient.graphql({
+    query: mutations.createTodo,
+    variables: {
+      input: {
+        name: formData.get('name')?.toString() ?? ''
+      }
+    }
+  });
+
+  revalidatePath('/');  
+}
+
+export default async function Home() {
+  const { data, errors } = await cookiesClient.graphql({
+    query: queries.listTodos
+  });
+
+  const todos = data.listTodos.items;
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <div
+        style={{
+          maxWidth: '500px',
+          margin: '0 auto',
+          textAlign: 'center',
+          marginTop: '100px'
+        }}
+      >
+        <form action={createTodo}>
+          <input name="name" placeholder='Add a todo' />
+          <button type="submit">Add</button>
+        </form>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {(!todos || todos.length === 0 || errors) && (
+          <div>
+            <p>No todos, please add one.</p>
+          </div>
+        )}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <ul>
+          {todos.map((todo) => {
+            return <li style={{ listStyle: 'none' }}>{todo.name}</li>;
+          })}
+        </ul>
       </div>
     </main>
-  )
+  );
 }
